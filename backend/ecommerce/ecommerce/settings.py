4a4f8f7c781591f92f31c29b0440775d08fbabcd
aswathy <aswathyathejas@gmail.com>
2026-06-04 12:22:07 +0530

    migrations

--- backend/ecommerce/ecommerce/settings.py
+++ backend/ecommerce/ecommerce/settings.py
@@ -215,7 +215,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     
-    'AUTH_HEADER_TYPES': ('JWT',),
+    'AUTH_HEADER_TYPES': ('Bearer',),
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=8),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'BLACKLIST_AFTER_ROTATION': True,
