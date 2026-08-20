3d8a39828ec1b97485122db1927921c4e17bbe18
sooryah <sooryakr2004@gmail.com>
2026-08-20 16:49:19 +0530

    Add localhost:8000 to CORS allowed origins

--- backend/ecommerce/ecommerce/settings.py
+++ backend/ecommerce/ecommerce/settings.py
@@ -241,6 +241,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://carooa.com",
     "http://carooa.com",
     "http://localhost:3000",
+    "http://localhost:8000",
 ]
 CSRF_TRUSTED_ORIGINS = [
     "https://carooa.com",
